c21a6642f0bce04669b992b6d94d91b526f6fe6f
Joaquin <74403807+LordMrMittens@users.noreply.github.com>
2023-01-26 11:57:56 +0000

    Market Dayshift Job Assigned

--- Assets/Scripts/Characters/JobModuleShopKeeper.cs
+++ Assets/Scripts/Characters/JobModuleShopKeeper.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class JobModuleShopKeeper : BaseJobModule
 {
-    [SerializeField] float stockCheckTimer;
+    float stockCheckTimer;
     [SerializeField] float stockCheckFrequency;
 
 
